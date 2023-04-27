70c5848c0fed35ca9c5705cb6e17bdb2e759bce7
Sergio Garcia <sergio@atratopago.com>
2023-04-27 16:14:25 +0200

    starting cpp06

---
+++ cpp_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: segarcia <segarcia@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 14:58:26 by segarcia          #+#    #+#             */
+/*   Updated: 2023/04/27 14:59:22 by segarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+class ScalarConverter {
+public:
+  ScalarConverter();
+  ScalarConverter(const ScalarConverter &other);
+  ScalarConverter &operator=(const ScalarConverter &other);
+  ~ScalarConverter();
+
+private:
+};
+
+#endif // !1
