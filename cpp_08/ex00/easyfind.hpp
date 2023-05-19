7157375edf86fb6b1c8743989c4a15cd13ed59ba
Sergio Garcia <sergio@atratopago.com>
2023-05-19 16:47:55 +0200

    ckpt cpp08

---
+++ cpp_08/ex00/easyfind.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: segarcia <segarcia@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 11:28:31 by segarcia          #+#    #+#             */
+/*   Updated: 2023/05/19 12:47:49 by segarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <list>
+#include <set>
+#include <vector>
+
+// If value is not found in container of <int>, we return -1;
+// Else we return the position of the first ocurrance.
+
+template <typename T> int easyfind(const T &cont, const int val) {
+  int idx = -1;
+  try {
+    typename T::const_iterator it = std::find(cont.begin(), cont.end(), val);
+    if (it != cont.end())
+      idx = std::distance(cont.begin(), it);
+  } catch (const std::exception &) {
+    return (-1);
+  }
+  return (idx);
+}
---
+++ cpp_08/ex00/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: segarcia <segarcia@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 11:28:27 by segarcia          #+#    #+#             */
+/*   Updated: 2023/05/19 12:50:32 by segarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main(void) {
+  std::vector<int> num_vector;
+  num_vector.push_back(10);
+  num_vector.push_back(20);
+  num_vector.push_back(30);
+
+  const std::vector<int> const_num_vector(num_vector);
+  std::cout << "num_vector[0] = " << num_vector[0] << std::endl;
+  std::cout << "num_vector[1] = " << num_vector[1] << std::endl;
+  std::cout << "num_vector[2] = " << num_vector[2] << std::endl;
+
+  std::cout << "const_num_vector[0] = " << const_num_vector[0] << std::endl;
+  std::cout << "const_num_vector[1] = " << const_num_vector[1] << std::endl;
+  std::cout << "const_num_vector[2] = " << const_num_vector[2] << std::endl;
+
+  std::cout << "value 10 is in idx: " << easyfind(num_vector, 10) << std::endl;
+  std::cout << "value 20 is in idx: " << easyfind(num_vector, 20) << std::endl;
+  std::cout << "value 30 is in idx: " << easyfind(const_num_vector, 30)
+            << std::endl;
+  std::cout << "value 40 is in idx: " << easyfind(const_num_vector, 40)
+            << std::endl;
+
+  std::list<int> num_list;
+  num_list.push_back(100);
+  num_list.push_back(200);
+  num_list.push_back(300);
+  std::cout << "value 100 is in idx: " << easyfind(num_list, 100) << std::endl;
+
+  std::set<int> num_set;
+  num_set.insert(90);
+  num_set.insert(80);
+  num_set.insert(70);
+
+  std::cout << "num_set: " << *num_set.begin() << std::endl;
+  std::cout << "num_set: " << *std::next(num_set.begin()) << std::endl;
+  std::cout << "num_set: " << *num_set.begin() << std::endl;
+
+  std::cout << "value 80 is in idx: " << easyfind(num_set, 80) << std::endl;
+  return (1);
+}
